01401fc69905d104da2437f80c719f459df381c2
JaStPo <j.stehlmann@sts-lurup.de>
2024-09-23 12:46:04 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showNumber(pins.digitalReadPin(DigitalPin.P0))
+    basic.showNumber(pins.analogReadPin(AnalogPin.P1))
 })
 basic.forever(function () {
-    if (pins.analogReadPin(AnalogPin.P1) > 500) {
+    pins.digitalWritePin(DigitalPin.P0, 1)
+    if (pins.analogReadPin(AnalogPin.P1) > 300) {
         basic.showIcon(IconNames.Happy)
         basic.setLedColor(0x00ff00)
+        pins.digitalWritePin(DigitalPin.C16, 0)
     } else {
         basic.showIcon(IconNames.Sad)
         basic.setLedColor(0xff0000)
+        pins.digitalWritePin(DigitalPin.C16, 1)
     }
 })
