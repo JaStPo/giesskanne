aa5b0eb0324436741f56f1977449ebb9c96de298
JaStPo <j.stehlmann@sts-lurup.de>
2024-09-26 20:16:23 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,14 @@
 input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    basic.showNumber(input.lightLevel())
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    basic.showNumber(input.temperature())
+    basic.showNumber(pins.analogReadPin(AnalogPin.P0))
 })
 basic.forever(function () {
     if (pins.analogReadPin(AnalogPin.P0) > 300) {
         basic.showIcon(IconNames.Happy)
         basic.setLedColor(0x00ff00)
-        pins.analogWritePin(AnalogPin.C16, 0)
+        pins.analogWritePin(AnalogPin.C12, 0)
     } else {
         basic.showIcon(IconNames.Sad)
         basic.setLedColor(0xff0000)
-        pins.analogWritePin(AnalogPin.C16, 1023)
-    }
-})
-basic.forever(function () {
-    if (input.lightLevel() < 300) {
-        basic.showLeds(`
-            . # # . .
-            # # . . .
-            # # . . .
-            # # . . .
-            . # # . .
-            `)
         pins.analogWritePin(AnalogPin.C12, 1023)
-    } else {
-        pins.analogWritePin(AnalogPin.C12, 5)
     }
 })
